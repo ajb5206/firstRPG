d02ee576f4e4ef1c5e412094e3a5274dc2cb0887
Alex Bertotto <ajb5206@gmail.com>
2024-02-26 12:56:41 -0800

    update buyWeapon function

--- script.js
+++ script.js
@@ -100,7 +100,13 @@ function buyHealth(){
 }
 
 function buyWeapon(){
-
+	if(gold >= 30){
+		gold -= 30;
+		currentWeapon ++;
+		goldText.innerText = gold;
+		let newWeapon = weapons[currentWeapon].name;
+		text.innerText = "You now have a " + newWeapon + ".";
+	}
 }
 
 function fightSlime() {
