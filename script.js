7e0faf7b20485cefe7a6483d50dde7b1a6fb53a7
Alex Bertotto <ajb5206@gmail.com>
2024-02-27 10:56:48 -0800

    add fight to locations

--- script.js
+++ script.js
@@ -73,6 +73,12 @@ const locations = [
 		"button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
 		"button functions": [fightSlime, fightBeast, goTown],
 		text: "You enter the cave. You see some monsters."
+	},
+	{
+		name: "fight",
+		"button text": ["Attack", "Dodge", "Run"],
+		"button functions": [attack, dodge, goTown],
+		text: "You are fighting a monster."
 	}
 ];
 
@@ -162,5 +168,13 @@ function fightDragon(){
 }
 
 function goFight(){
+
+}
+
+function attack(){
+
+}
+
+function dodge(){
 
 }
