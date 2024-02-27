610d969a7ef3d7e4e557636df394b4a4798d3db1
Alex Bertotto <ajb5206@gmail.com>
2024-02-27 11:29:12 -0800

    add text to dodge function

--- script.js
+++ script.js
@@ -190,7 +190,7 @@ function attack(){
 }
 
 function dodge(){
-
+	text.innerText = "You dodge the attack from the " + monsters[fighting].name + ".";
 }
 
 function defeatMonster(){
@@ -198,5 +198,5 @@ function defeatMonster(){
 }
 
 function lose(){
-	
+
 }
