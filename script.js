b03d414a25c2a03241f1923e39176d48e36f924e
Alex Bertotto <ajb5206@gmail.com>
2024-02-26 18:51:01 -0800

    add conditonal to buyWeapon to see which weapon the user has and give an option to sell if they have the best weapon

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ function buyHealth(){
 }
 
 function buyWeapon(){
-	if(currentWeapon < 3){
+	if(currentWeapon < weapons.length - 1){
 		if(gold >= 30){
 			gold -= 30;
 			currentWeapon ++;
@@ -110,8 +110,12 @@ function buyWeapon(){
 			inventory.push(newWeapon);
 			text.innerText += " In your inventory you have: " + inventory;
 		}else{
-			text.innerText = "You do not have enough gold to buy a weapon."
+			text.innerText = "You do not have enough gold to buy a weapon.";
 		}
+	}else{
+		button2.innerText = "Sell weapon for 15 gold";
+		button2.onclick = sellWeapon;
+		text.innerText = "You already have the most powerful weapon!";
 	}
 }
 
